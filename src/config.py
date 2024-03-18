4d0a10f12672ba19f4bc364d92a46e6d3bf61a52
andabele1 <andabele10@gmail.com>
2024-03-18 10:53:37 -0500

    correccion

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ class DevelopmentConfig():
     DEBUG = True
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'Andres'
-    MYSQL_PASSWORD = 'Andabele1*'
-    MYSQL_DB = 'api_'
+    MYSQL_PASSWORD = 'Andres1234'
+    MYSQL_DB = 'api'
 
 config = {
     'development': DevelopmentConfig
